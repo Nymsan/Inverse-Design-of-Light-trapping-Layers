579e49d3af0c01a04e5b857d8e728d14cbcadcea
Andreas Nymand <andnymand@gmail.com>
2026-06-03 09:01:30 +0200

    more scripts

--- Scripts/submit_sinc_test_3d.sh
+++ Scripts/submit_sinc_test_3d.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#BSUB -J gen_curves_sinc_test
-#BSUB -o logs/gen_curves_sinc_test.out
-#BSUB -e logs/gen_curves_sinc_test.err
+#BSUB -J gen_curves_sinc_test_3d
+#BSUB -o logs/gen_curves_sinc_test_3d.out
+#BSUB -e logs/gen_curves_sinc_test_3d.err
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 4
@@ -19,21 +19,23 @@ echo "Job starting on $(hostname)"
 
 echo "Running WITH subpixel smoothing..."
 uv run generate_curve.py \
-    --name "sweep_sinc_test_subpixel" \
-    --params_x "40,0;40,0;40,0;40,0;40,0;40,0;40,0;40,0;40,0;40,0" \
-    --order_N 1 5 10 20 35 50 100 \
+    --name "sweep_sinc_test_3d_subpixel" \
+    --params_x "10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0" \
+    --params_y "10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0" \
+    --order_N 1 3 5 8 10 12 15 \
     --num_layers 1 2 3 5 10 15 20 25 50 100 150 250 251 \
     --wavelengths 700 700 1 \
-    --nx 5000 \
-    --ny 1
+    --nx 500 \
+    --ny 500
 
 echo "Running WITHOUT subpixel smoothing..."
 uv run generate_curve.py \
-    --name "sweep_sinc_test_no_subpixel" \
+    --name "sweep_sinc_test_3d_no_subpixel" \
     --no_subpixel \
-    --params_x "40,0;40,0;40,0;40,0;40,0;40,0;40,0;40,0;40,0;40,0" \
-    --order_N 1 5 10 20 35 50 100 \
+    --params_x "10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0" \
+    --params_y "10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0;10,0" \
+    --order_N 1 3 5 8 10 12 15 \
     --num_layers 1 2 3 5 10 15 20 25 50 100 150 250 251 \
     --wavelengths 700 700 1 \
-    --nx 5000 \
-    --ny 1
+    --nx 500 \
+    --ny 500
